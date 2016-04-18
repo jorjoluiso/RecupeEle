75bff1069ee0df021a4e05269e117a41a7757585
jorjoluiso <jorjoluiso@gmail.com>
2016-04-17 21:28:03 -0500

    Añadida la conexión a la base de datos Oracle

---
+++ BaseOracle.py
@@ -0,0 +1,52 @@
+import cx_Oracle
+
+
+class BaseOracle(object):
+
+    def __init__(self, host="localhost", user="", password="", sid=""):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.sid = sid
+        self.cursor = None
+
+    def conectar(self):
+        try:
+            #con = cx_Oracle.connect('armando/a@127.0.0.1/xe')
+            self.db = cx_Oracle.connect(self.user + '/' + self.password + '@127.0.0.1/XE')
+            print(("Conectado a la base Oracle " + (self.db.version)))
+            self.cursor = self.db.cursor()
+        except cx_Oracle.DatabaseError as e:
+            error, = e.args
+            if error.code == 1017:
+                print('Please check your credentials.')
+            else:
+                print(('Database connection error: %s'.format(e)))
+            # Very important part!
+            raise
+
+    def desconectar(self):
+        try:
+            self.cursor.close()
+            self.db.close()
+            print("Desconectado de la base Oracle")
+        except cx_Oracle.DatabaseError:
+            pass
+
+    #Ejecuta sentencias de actualizaciones en la base de datos
+    def ejecutar(self, sql):
+        try:
+            self.cursor.execute(sql)
+            self.db.commit()
+        except cx_Oracle.DatabaseError as e:
+            error, = e.args
+            if error.code == 955:
+                print('Table already exists')
+            elif error.code == 1031:
+                print("Insufficient privileges")
+            print((error.code))
+            print((error.message))
+            print((error.context))
+
+            # Raise the exception.
+            raise
--- __init__.py
+++ __init__.py
@@ -1,24 +1,11 @@
 # -*- coding: utf-8 -*-
-#import parseXMLeni as p
-from parseXMLFactura import *
-
+from ParseXMLFactura import *
+from CargaFactura import *
 
 if __name__ == "__main__":
-
-        #ce = ComprobanteElectronico('FACTURA')
-        #p.extraerDatosFactura("0704201601109170775200120010030030019921234567817.xml", ce)
-        #oracle = CargarBaseOracle("localhost", "armando", "a")
-        #oracle.connect()
-        ##oracle.execute("select sysdate from dual")
-
-        #print((ce.autorizacion))
-        #oracle.executetran("INSERT INTO ELE_DOCUMENTOS VALUES (s_ele_documentos.nextval,'"
-        #+ ce.autorizacion + "','" + ce.documento + "','" + ce.razonSocial + "','"
-        #+ ce.nombreComercial + "','" + ce.direccion + "','" + ce.establecimiento + "','"
-        #+ ce.puntoEmision + "','" + ce.secuencial + "',TO_DATE('" + ce.fechaEmision
-        #+ "', 'dd/mm/yyyy'),'" + ce.autorizacion + "','" + ce.tipo + "')")
-        #oracle.execute("select * from ELE_DOCUMENTOS")
-        #oracle.disconnect()
-        parseFactura = parseXMLFactura()
-        parseFactura.getFactura("0704201601109170775200120010030030019921234567817.xml")
+        parseFactura = ParseXMLFactura()
+        factura = parseFactura.getFactura("0704201601109170775200120010030030019921234567817.xml")
+        #Imprime objeto Factura
         parseFactura.imprimir()
+        carga = CargaFactura()
+        carga.carga(factura)
