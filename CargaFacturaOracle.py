b2d8cfbce7962ffa3db915d9b731fdc79f2b3111
jorjoluiso <jorjoluiso@gmail.com>
2016-04-17 21:34:53 -0500

    Update nombre de archivo

---
+++ CargaFacturaOracle.py
@@ -0,0 +1,18 @@
+from BaseOracle import *
+from Factura import *
+
+
+class CargaFacturaOracle(object):
+
+    def __init__(self):
+        pass
+
+    def carga(self, factura):
+        oracle = BaseOracle("localhost", "armando", "a")
+        oracle.conectar()
+        oracle.ejecutar("INSERT INTO ELE_DOCUMENTOS VALUES (s_ele_documentos.nextval,'"
+        + factura.autorizacion + "','" + factura.documento + "','" + factura.razonSocial + "','"
+        + factura.nombreComercial + "','" + factura.direccion + "','" + factura.establecimiento + "','"
+        + factura.puntoEmision + "','" + factura.secuencial + "',TO_DATE('" + factura.fechaEmision
+        + "', 'dd/mm/yyyy'),'" + factura.autorizacion + "','" + factura.tipo + "')")
+        oracle.desconectar()
--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from ParseXMLFactura import *
-from CargaFactura import *
+from CargaFacturaOracle import *
 
 if __name__ == "__main__":
         parseFactura = ParseXMLFactura()
         factura = parseFactura.getFactura("0704201601109170775200120010030030019921234567817.xml")
         #Imprime objeto Factura
         parseFactura.imprimir()
-        carga = CargaFactura()
+        carga = CargaFacturaOracle()
         carga.carga(factura)
