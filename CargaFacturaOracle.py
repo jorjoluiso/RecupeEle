4de13f47679d758601c8d0dc9ff726dc5019b18b
jorjoluiso <jorjoluiso@gmail.com>
2016-04-18 09:37:14 -0500

    Ingresa detalle en base de datos Oracle

--- CargaFacturaOracle.py
+++ CargaFacturaOracle.py
@@ -10,9 +10,24 @@ class CargaFacturaOracle(object):
     def carga(self, factura):
         oracle = BaseOracle("localhost", "armando", "a")
         oracle.conectar()
-        oracle.ejecutar("INSERT INTO ELE_DOCUMENTOS VALUES (s_ele_documentos.nextval,'"
-        + factura.autorizacion + "','" + factura.documento + "','" + factura.razonSocial + "','"
+
+        oracle.ejecutar("delete ELE_DOCUMENTOS")
+
+        oracle.ejecutar("INSERT INTO ELE_DOCUMENTOS VALUES ('"
+        + factura.claveAcceso + "','" + factura.documento + "','" + factura.razonSocial + "','"
         + factura.nombreComercial + "','" + factura.direccion + "','" + factura.establecimiento + "','"
         + factura.puntoEmision + "','" + factura.secuencial + "',TO_DATE('" + factura.fechaEmision
         + "', 'dd/mm/yyyy'),'" + factura.autorizacion + "','" + factura.tipo + "')")
+
+        i = 1
+        for d in factura.detalle:
+            oracle.ejecutar("INSERT INTO ELE_FACTURA_DETALLES"
+            + "(CLAVE_ACCESO_ELE_DOCUMENTOS,NUMFILA,CODIGO_PRINCIPAL,DESCRIPCION,CANTIDAD,"
+            + "PRECIO_UNITARIO,DESCUENTO,PRECIO_TOTAL_SIN_IMPUESTO)"
+            + "VALUES ('" + factura.claveAcceso + "'," + str(i) + ",'" + d.codigoPrincipal + "','"
+            + d.descripcion + "'," + str(d.cantidad) + "," + str(d.precioUnitario) + ","
+            + str(d.descuento) + ","
+            + str(d.total) + ")")
+            i = i + 1
+
         oracle.desconectar()
